1b2c9dfaed792054291877f3bfe3fa31c5963b86
Chriztian Manzano <cristianorb@unicauca.edu.co>
2022-01-11 19:28:47 -0500

    fix: Add file .gitignore

--- AkantarApp/settings.py
+++ AkantarApp/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hsm_o4vmdepof)d2fmaaszm7j_a56n$b=)e#ovrti38b8#=!ts'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
